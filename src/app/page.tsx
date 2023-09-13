44487caf6f7c50cd8bc177f208fa2da15d1b59f2
anhtrvn <anhtran18202@gmail.com>
2023-09-12 20:26:50 -0500

    #Temporary Quote and Profile Forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Profile from "./components/Profile";
+import QuoteForm from "./components/QuoteForm";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Hero />
+      {/* <Hero /> */}
+      {/* <Profile /> */}
+      <QuoteForm />
     </div>
   );
 }
