1b361eecd9f332d19147058792b98d074b47b3b5
alimvasaya <alimvasaya1@gmail.com>
2023-09-12 20:38:37 -0500

    Form Page edit

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,35 @@
+// pages/forum.tsx
+"use client";
+import React, { useState } from "react";
+
+export default function Form() {
+  const [posts, setPosts] = useState<string[]>([]);
+  const [newPost, setNewPost] = useState<string>("");
+
+  const handleAddPost = () => {
+    if (newPost.trim() !== "") {
+      setPosts([...posts, newPost]);
+      setNewPost("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Forum</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your post..."
+          value={newPost}
+          onChange={(e) => setNewPost(e.target.value)}
+        />
+        <button onClick={handleAddPost}>Add Post</button>
+      </div>
+      <div>
+        {posts.map((post, index) => (
+          <div key={index}>{post}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Form from "./components/Form";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+    </div>
+  );
+}
+
+export function Forum() {
+  return (
+    <div>
+      <Form />
     </div>
   );
 }
