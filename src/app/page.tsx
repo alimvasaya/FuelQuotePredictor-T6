e3f93e18d4cf8d7678be1a76a313312b1d2e29f5
alimvasaya <alimvasaya1@gmail.com>
2023-09-19 16:50:51 -0500

    Admin Portal for Fuel Quote Application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Admin from "./components/Admin/page";
-import Form from "./components/Forum/page";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Link from "next/link";
-import QuoteHistory from "./components/Fuel_Quote_History/page";
-import Clinet_profiles from "./components/Client_profiles/page";
-import Analytics from "./components/Analytics/page";
+
 
 
 export default function Home() {
@@ -14,14 +11,6 @@ export default function Home() {
       <Header />
       {<Hero /> }
       {/* <Profile />*/}
-      <Link href="components#Forum"></Link>
-      <Form />
-      <Link href="components#Admin"></Link>
-      <Admin />
-      <QuoteHistory />
-      <Link href="components#Client_profiles"></Link>
-      <Clinet_profiles />
-
     </div>
   );
 }
