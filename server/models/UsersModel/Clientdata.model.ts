94df3d9e685b7bf47aa6703b6cd2021c6a58eac9
alimvasaya <alimvasaya1@gmail.com>
2023-10-12 15:32:45 -0500

    Adding fuel quote modules and history

---
+++ server/models/UsersModel/Clientdata.model.ts
@@ -0,0 +1,52 @@
+// const clientInfo = {
+//     userID: {
+//         type: "string",
+//     },
+//     name: {
+//         type: "string",
+//         required: true,
+//         trim: true,
+//         maxlength: 50,
+//     },
+//     address1: {
+//         type: "string",
+//         required: true,
+//         trim: true,
+//         maxlength: 100,
+//     },
+//     address2: {
+//         type: "string",
+//         required: false,
+//         trim: true,
+//         maxlength: 100,
+//     },
+//     city: {
+//         type: "string",
+//         required: true,
+//         trim: true,
+//         maxlength: 100,
+//     },
+//     state: {
+//         type: "string",
+//         required: true,
+//         trim: true,
+//         maxlength: 2,
+//     },
+//     zipcode: {
+//         type: "number",
+//         required: true,
+//         trim: true,
+//         maxlength: 9,
+//         minlength: 5,
+//     },
+// };
+
+export const clientInfo ={
+    userID: "1",
+    name: "John Smith",
+    address1: "123 Main St",
+    address2: "Apt 1",
+    city: "Houston",
+    state: "TX",
+    zipcode: 77001,
+};
